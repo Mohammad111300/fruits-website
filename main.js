6c77aa609d68cc9df486f9b78aaf89ab39b88548
Mohammad111300 <155152138+Mohammad111300@users.noreply.github.com>
2023-12-29 19:10:45 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    //autoplay: {
+    //  delay: 2500,
+    //  disableOnInteraction: false,
+    //},
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
