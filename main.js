50232e53f450892e4a9724c0a98c44247e1be485
Mohammad111300 <155152138+Mohammad111300@users.noreply.github.com>
2023-12-29 20:56:56 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
-    //autoplay: {
-    //  delay: 2500,
-    //  disableOnInteraction: false,
-    //},
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+  let menu = document.getElementById("menu-icon");
+  let navbar = document.getElementById("navbar");
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+  }
+  window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
